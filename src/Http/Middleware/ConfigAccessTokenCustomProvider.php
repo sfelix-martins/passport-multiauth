484f5acc42e074bc9c6eb4ef32f13230cc1e1e19
Samuel <samuelmartins@negociosreais.com.br>
2018-02-02 21:52:29 -0200

    Added class to handle with Guards

--- src/Http/Middleware/ConfigAccessTokenCustomProvider.php
+++ src/Http/Middleware/ConfigAccessTokenCustomProvider.php
@@ -3,7 +3,6 @@
 namespace SMartins\PassportMultiauth\Http\Middleware;
 
 use Closure;
-use Illuminate\Support\Str;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\App;
 use Illuminate\Auth\CreatesUserProviders;
@@ -13,6 +12,7 @@ use League\OAuth2\Server\ResourceServer;
 use League\OAuth2\Server\Exception\OAuthServerException;
 use Symfony\Bridge\PsrHttpMessage\Factory\DiactorosFactory;
 use SMartins\PassportMultiauth\ProviderRepository;
+use SMartins\PassportMultiauth\Guards\GuardChecker;
 
 class ConfigAccessTokenCustomProvider
 {
@@ -63,12 +63,12 @@ class ConfigAccessTokenCustomProvider
             );
 
             // If just one entity a register with this id follow the flow
-            if (! $entities->count() > 1) {
+            if (! ($entities->count() > 1)) {
                 return $next($request);
             }
 
             // Get the auth guard if has to check the default guard
-            $guards = $this->getAuthGuards($request);
+            $guards = GuardChecker::getAuthGuards($request);
 
             if (count($guards) == 0) {
                 return $next($request);
@@ -84,7 +84,7 @@ class ConfigAccessTokenCustomProvider
 
             // If has users with same id and the accessToken provider is different
             // of default provider return unauthenticated.
-            if ($accessToken->provider != $this->defaultGuardProvider($guards[0])) {
+            if ($accessToken->provider != GuardChecker::defaultGuardProvider($guards[0])) {
                 throw new AuthenticationException("Unauthenticated", $guards);
             }
         } catch (OAuthServerException $e) {
@@ -94,35 +94,18 @@ class ConfigAccessTokenCustomProvider
         return $next($request);
     }
 
-    public function getAuthGuards(Request $request)
-    {
-        $middlewares = $request->route()->middleware();
-
-        $guards = [];
-        foreach ($middlewares as $middleware) {
-            if (Str::startsWith($middleware, 'auth')) {
-                $explodedGuards = explode(',', Str::after($middleware, ':'));
-                $guards = array_unique(array_merge($guards, $explodedGuards));
-            }
-        }
-
-        return $guards;
-    }
-
     public function entitiesWithSameIdOnProviders($id): Collection
     {
         $providers = array_keys(config('auth.providers'));
         $entities = collect([]);
         foreach ($providers as $provider) {
-            $entities->push($this->findEntityOnProvider($provider, $id));
+            $entity = $this->findEntityOnProvider($provider, $id);
+            if (! is_null($entity)) {
+                $entities->push($entity);
+            }
         }
 
         return $entities;
-    }
-
-    public function defaultGuardProvider(string $guard)
-    {
-        return config('auth.guards.'.$guard.'.provider');
     }
 
     public function findEntityOnProvider($provider, $userId)
