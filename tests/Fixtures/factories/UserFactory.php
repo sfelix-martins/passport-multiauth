97e2fbe4fd5468c043c4cd251132b4869b638bb8
Samuel Martins <sfelix_martins@hotmail.com>
2019-11-08 23:16:06 -0300

    [6.x] Updated to compatibility with Laravel ^6.0 and Passport ^8.0

--- tests/Fixtures/factories/UserFactory.php
+++ tests/Fixtures/factories/UserFactory.php
@@ -1,6 +1,8 @@
 <?php
 
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+use SMartins\PassportMultiauth\Tests\Fixtures\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,11 +15,11 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(\SMartins\PassportMultiauth\Tests\Fixtures\Models\User::class, function (Faker $faker) {
+$factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
+        'remember_token' => Str::random(10),
     ];
 });
